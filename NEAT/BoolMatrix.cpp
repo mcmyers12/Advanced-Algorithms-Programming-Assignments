ded811279d20327cc6ae8b63ceaede9ba4630b9d
mcmyers12 <mirandacmyers12@gmail.com>
2015-04-13 20:18:24 -0700

    Added test data

--- NEAT/BoolMatrix.cpp
+++ NEAT/BoolMatrix.cpp
@@ -1,18 +1,29 @@
+//
+//  BoolMatrix.cpp
+//  AlgorithmsWarshallAndFloyd
+//
+//  Created by Miranda Myers on 4/13/15.
+//  Copyright (c) 2015 Miranda Myers. All rights reserved.
+//
 
 #include "BoolMatrix.h"
 
-BoolMatrix::BoolMatrix(bool** adjacencyMatrixIn)
+
+#include "BoolMatrix.h"
+
+BoolMatrix::BoolMatrix(bool** adjacencyMatrixIn, int sizeIn)
 {
     adjacencyMatrix = adjacencyMatrixIn;
+    size = sizeIn;
 }
 
 
 BoolMatrix::~BoolMatrix()
 {
-
+    
 }
 
-//R(k)[i, j] ← R(k−1)[i, j] or (R(k−1)[i, k] and R(k−1)[k, j])        
+//R(k)[i, j] ← R(k−1)[i, j] or (R(k−1)[i, k] and R(k−1)[k, j])
 //Completes one step of Warshall's Algorithm
 void BoolMatrix::WarshallStep(int k, int i, int j)
 {
@@ -21,17 +32,18 @@ void BoolMatrix::WarshallStep(int k, int i, int j)
 
 
 /*
-ALGORITHM Warshall(A[1..n, 1..n])
-//Implements Warshall’s algorithm for computing the transitive closure
-//Input: The adjacency matrix A of a digraph with n vertices //Output: The transitive closure of the digraph
-R(0) ← A
-for k ← 1 to n do
-for i ← 1 to n do 
-forj ←1tondo
-R(k)[i, j] ← R(k−1)[i, j] or (R(k−1)[i, k] and R(k−1)[k, j]) return R(n)
-*/
+ ALGORITHM Warshall(A[1..n, 1..n])
+ //Implements Warshall’s algorithm for computing the transitive closure
+ //Input: The adjacency matrix A of a digraph with n vertices //Output: The transitive closure of the digraph
+ R(0) ← A
+ for k ← 1 to n do
+ for i ← 1 to n do
+ forj ←1tondo
+ R(k)[i, j] ← R(k−1)[i, j] or (R(k−1)[i, k] and R(k−1)[k, j]) return R(n)
+ */
 void BoolMatrix::Warshall()//bool** adjacencyMatrix, bool** transitiveClosure)
 {
+    printMatrix();
     for (int k = 0; k < size; k++)
     {
         for (int i = 0; i < size; i++)
@@ -39,9 +51,10 @@ void BoolMatrix::Warshall()//bool** adjacencyMatrix, bool** transitiveClosure)
             for (int j = 0; j < size; j++)
             {
                 WarshallStep(k, i, j);
-                printMatrix();
+                
             }
         }
+        printMatrix();
     }
     //transitiveClosure = adjacencyMatrix;
 }
@@ -55,36 +68,42 @@ void BoolMatrix::printMatrix()
             cout << adjacencyMatrix[k][i] << " ";
         }
         cout << endl;
-    } 
+    }
+    cout << endl;
 }
 
 int main()
 {
-    cout << "hi" << endl << endl;
-    int **adjacencyMatrix;
-    p = new int*[4]; // dynamic `array (size 4) of pointers to int`
-
-    for (int i = 0; i < 4; ++i) 
+    bool **adjacencyMatrix;
+    adjacencyMatrix = new bool*[4]; // dynamic `array (size 4) of pointers to int`
+    
+    for (int i = 0; i < 4; ++i)
     {
-        adjacencyMatrix[i] = new int[4];
+        adjacencyMatrix[i] = new bool[4];
     }
     
-    for (int i = 0; i < 4; ++i) 
-    {
-        for (int j = 0; j < 4; ++j)
-            adjacencyMatrix[i][j] = 1;
-    } 
+    adjacencyMatrix[0][0] = 0;
+    adjacencyMatrix[0][1] = 1;
+    adjacencyMatrix[0][2] = 0;
+    adjacencyMatrix[0][3] = 0;
+    adjacencyMatrix[1][0] = 0;
+    adjacencyMatrix[1][1] = 0;
+    adjacencyMatrix[1][2] = 0;
+    adjacencyMatrix[1][3] = 1;
+    adjacencyMatrix[2][0] = 0;
+    adjacencyMatrix[2][1] = 0;
+    adjacencyMatrix[2][2] = 0;
+    adjacencyMatrix[2][3] = 0;
+    adjacencyMatrix[3][0] = 1;
+    adjacencyMatrix[3][1] = 0;
+    adjacencyMatrix[3][2] = 1;
+    adjacencyMatrix[3][3] = 0;
     
-    /*
-    bool adjacencyMatrix[4][4] = {  
-        {0, 1, 0, 0} ,   /*  initializers for row indexed by 0 */
-        {0, 0, 0, 1} ,
-        {0, 0, 0, 0} ,   /*  initializers for row indexed by 1 */
-        {1, 0, 1, 0}   /*  initializers for row indexed by 2 */
-    };*/
     
-    BoolMatrix boolMatrix(adjacencyMatrix);
-    boolMatrix.Warshall();
-    return 0;
+
+
+BoolMatrix boolMatrix(adjacencyMatrix, 4);
+boolMatrix.Warshall();
+return 0;
 }
 
--- NEAT/BoolMatrix.h
+++ NEAT/BoolMatrix.h
@@ -1,20 +1,35 @@
+//
+//  BoolMatrix.h
+//  AlgorithmsWarshallAndFloyd
+//
+//  Created by Miranda Myers on 4/13/15.
+//  Copyright (c) 2015 Miranda Myers. All rights reserved.
+//
+
+#ifndef __AlgorithmsWarshallAndFloyd__BoolMatrix__
+#define __AlgorithmsWarshallAndFloyd__BoolMatrix__
+
+#include <stdio.h>
+
 
 #include <iostream>
 using namespace std;
 
 class BoolMatrix
 {
-    public:
-        BoolMatrix(bool (&adjacencyMatrixIn)[size][size]);
-        ~BoolMatrix();
-        
-        //Completes one step of Warshall's Algorithm
-        void WarshallStep(int k, int i, int j);
-        void Warshall();//bool** adjacencyMatrix, bool** transitiveClosure);
-        void printMatrix();
-        
-    private:
-        //static const int MAX_SIZE = 100;
-        int size;
-        bool** adjacencyMatrix;
+public:
+    BoolMatrix(bool** adjacencyMatrixIn, int sizeIn);
+    ~BoolMatrix();
+    
+    //Completes one step of Warshall's Algorithm
+    void WarshallStep(int k, int i, int j);
+    void Warshall();//bool** adjacencyMatrix, bool** transitiveClosure);
+    void printMatrix();
+    
+private:
+    //static const int MAX_SIZE = 100;
+    int size;
+    bool** adjacencyMatrix;
 };
+
+#endif /* defined(__AlgorithmsWarshallAndFloyd__BoolMatrix__) */
